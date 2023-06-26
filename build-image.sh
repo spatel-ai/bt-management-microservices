fbb2b6a5465cbb3d5fe555aefb881be33a716403
imshubhampatel <shubhampatel2024@gmail.com>
2023-06-26 17:45:35 +0530

    Modified : Fixed the minor changes

--- build-image.sh
+++ build-image.sh
@@ -1,13 +1,5 @@
 #!/bin/bash 
  
-cd spring-cloud-gateway && mvn clean install  && cd .. 
-cd authentication-service && mvn clean install  && cd .. 
-cd horizons-service && mvn clean install  && cd .. 
-cd naming-server && mvn clean install  && cd .. 
-cd config-server && mvn clean install  && cd .. 
-cd support-service && mvn clean install  && cd .. 
-cd students-connect-service && mvn clean install  && cd .. 
- 
 cd spring-cloud-gateway && chmod +x mvnw && ./mvnw spring-boot:build-image -DskipTests -e && cd .. 
 cd authentication-service && chmod +x mvnw && ./mvnw spring-boot:build-image -DskipTests -e && cd .. 
 cd horizons-service && chmod +x mvnw && ./mvnw spring-boot:build-image -DskipTests -e && cd .. 

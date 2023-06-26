2ccb28850d734f3d38c1a0e17ae9f4e019653d24
imshubhampatel <shubhampatel2024@gmail.com>
2023-06-26 17:07:47 +0530

    Minor changes

--- remove_images.sh
+++ remove_images.sh
@@ -7,4 +7,6 @@ docker rmi support-service:0.0.1-SNAPSHOT
 docker rmi authentication-service:0.0.1-SNAPSHOT
 docker rmi students-connect-service:0.0.1-SNAPSHOT
 docker rmi config-server:0.0.1-SNAPSHOT
+docker rmi paketobuildpacks/builder:base
+docker rmi paketobuildpacks/run:base-cnb
 docker images

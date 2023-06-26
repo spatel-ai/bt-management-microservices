67698e8a41822ea2dbf591f29851c28dc4dc611f
imshubhampatel <shubhampatel2024@gmail.com>
2023-06-26 16:49:35 +0530

    Added script : removing dummy images

---
+++ remove_images.sh
@@ -0,0 +1,10 @@
+
+docker images
+docker rmi naming-server:0.0.1-SNAPSHOT
+docker rmi spring-cloud-gateway-server:0.0.1-SNAPSHOT
+docker rmi horizons-service:0.0.1-SNAPSHOT
+docker rmi support-service:0.0.1-SNAPSHOT
+docker rmi authentication-service:0.0.1-SNAPSHOT
+docker rmi students-connect-service:0.0.1-SNAPSHOT
+docker rmi config-server:0.0.1-SNAPSHOT
+docker images

4d354e0dd071e34c9398f00e56800e789f949d43
imshubhampatel <shubhampatel2024@gmail.com>
2023-06-26 19:49:01 +0530

    Feat : Added Jenkins Pipeline

--- build-images.sh
+++ build-images.sh
@@ -6,4 +6,4 @@ cd horizons-service && chmod +x mvnw && ./mvnw spring-boot:build-image -DskipTes
 cd naming-server && chmod +x mvnw && ./mvnw spring-boot:build-image -DskipTests -e && cd .. 
 cd config-server && chmod +x mvnw && ./mvnw spring-boot:build-image -DskipTests -e && cd .. 
 cd support-service && chmod +x mvnw && ./mvnw spring-boot:build-image -DskipTests -e && cd .. 
-cd students-connect-service && chmod +x mvnw && ./mvnw spring-boot:build-image -DskipTests -e && cd .. 
+cd students-connect-service && chmod +x mvnw && ./mvnw spring-boot:build-image -DskipTests -e && cd ..

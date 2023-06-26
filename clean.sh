cc7e7be43fcfa78af2c1a3efda5b5f7981ce5e52
Shubham Patel <75959964+imshubhampatel@users.noreply.github.com>
2023-06-26 14:49:45 +0530

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,9 +1,8 @@
 #!/bin/bash 
- 
+cd naming-server && mvn clean install  && cd .. 
 cd spring-cloud-gateway && mvn clean install  && cd .. 
+cd config-server && mvn clean install  && cd ..
 cd authentication-service && mvn clean install  && cd .. 
 cd horizons-service && mvn clean install  && cd .. 
-cd naming-server && mvn clean install  && cd .. 
-cd config-server && mvn clean install  && cd .. 
 cd support-service && mvn clean install  && cd .. 
 cd students-connect-service && mvn clean install  && cd .. 

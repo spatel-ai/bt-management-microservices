661c21790798f0a2c6e041792b5fb33dcb8339ad
Pradeep8770 <pradeeppatel5148@gmail.com>
2023-07-08 21:01:39 +0530

    c

--- commit-bumb.sh
+++ commit-bumb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export commit_message="feat : added bumb $1"
 echo "${commit_message}"
-git commit -m $commit_message
+git commit -m "$commit_message"
 git log
 git push origin HEAD:Feature
 echo "Success"

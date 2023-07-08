ac69ab4a3c7a9fa2e007dd27d3f57b0614f8b825
Pradeep8770 <pradeeppatel5148@gmail.com>
2023-07-08 20:08:25 +0530

    c

--- commit-bumb.sh
+++ commit-bumb.sh
@@ -1,6 +1,6 @@
 export version=$1
 echo "I'm here"
 echo "${version}"
-git commit -m 'Feat : version bumb : ${version}'
+git commit -m "Feat : version bumb : ${version}"
 git push origin HEAD:Feature
 echo "Success"

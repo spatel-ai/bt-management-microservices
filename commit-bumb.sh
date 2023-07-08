946a698ad3257e7798217334101ba99c35761484
Pradeep8770 <pradeeppatel5148@gmail.com>
2023-07-08 20:56:21 +0530

    C

--- commit-bumb.sh
+++ commit-bumb.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 export commit_message=$1
 echo "${commit_message}"
-echo "I'm here"
-echo 'I'm here $1'
-echo 'version $1'
 git commit -m '$commit_message'
 git log
 git push origin HEAD:Feature

55faa47cdd79e732871c152cdebb19d1234817fa
Pradeep8770 <pradeeppatel5148@gmail.com>
2023-07-08 20:47:46 +0530

    c

--- commit-bumb.sh
+++ commit-bumb.sh
@@ -1,8 +1,9 @@
-
+#!/bin/bash
+commit_message = " Feat : version bumb $1"
 echo "I'm here"
 echo 'I'm here $1'
 echo "Version $1"
-'git commit -m "Feat : version bumb : $1"'
+'git commit -m "$commit_message"
 git log
 git push origin HEAD:Feature
 echo "Success"

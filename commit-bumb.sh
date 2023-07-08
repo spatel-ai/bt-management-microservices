c9bbd4eab107051abf901b5325bf15b5e34bc92c
Pradeep8770 <pradeeppatel5148@gmail.com>
2023-07-08 20:19:49 +0530

    modified:   commit-bumb.sh

--- commit-bumb.sh
+++ commit-bumb.sh
@@ -2,7 +2,7 @@
 echo "I'm here"
 echo "I'm here $1"
 echo "Version $1"
-# git commit -m "Feat : version bumb : $1"
+git commit -m "Feat : version bumb : $1"
 git log
 git push origin HEAD:Feature
 echo "Success"
